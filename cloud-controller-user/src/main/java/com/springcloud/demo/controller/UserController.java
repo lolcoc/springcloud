a543b03e07bf505a40c49d5fc86ecb4540a0b896
wbl <1762727456@qq.com>
2020-11-03 18:14:44 +0800

    consul

--- cloud-controller-user/src/main/java/com/springcloud/demo/controller/UserController.java
+++ cloud-controller-user/src/main/java/com/springcloud/demo/controller/UserController.java
@@ -1,11 +1,14 @@
-package com.springcloud.demo;
+package com.springcloud.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,22 +21,25 @@ import java.util.Map;
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private DiscoveryClient discoveryClient;
 
     /**
      * 保存
-     * @param user
+     * @param
      * @return
      */
     @ResponseBody
     @RequestMapping("/save")
-    public Map<String, Object> save(User user) {
-
-        Map<String, Object> resultMap = new HashMap<>(16);
+    public Map<String, Object> save() {
+        Map<String, Object> resultMap = new HashMap();
+        List<String> services = discoveryClient.getServices();
+        resultMap.put("success",services.get(0));
+        return resultMap;
+       /* Map<String, Object> resultMap = new HashMap<>(16);
 
         userService.save(user);
 
-        return resultMap;
+        return resultMap;*/
 
     }
 }
