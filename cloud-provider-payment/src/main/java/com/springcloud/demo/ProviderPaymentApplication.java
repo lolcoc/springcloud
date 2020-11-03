ad31678c15b5bc949b364c02454f8edf3318c46f
wbl <1762727456@qq.com>
2020-11-03 10:45:30 +0800

    项目初始化

--- cloud-provider-payment/src/main/java/com/springcloud/demo/ProviderPaymentApplication.java
+++ cloud-provider-payment/src/main/java/com/springcloud/demo/ProviderPaymentApplication.java
@@ -1,5 +1,14 @@
 package com.springcloud.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+@SpringBootConfiguration
 public class ProviderPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderPaymentApplication.class,args);
+    }
 }
