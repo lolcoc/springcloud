14c252c17858c4b7ac48df236597aeba7bd8019e
wbl <1762727456@qq.com>
2020-11-04 16:28:22 +0800

    openfeign

--- cloud-controller-user/src/main/java/com/springcloud/demo/service/UserService.java
+++ cloud-controller-user/src/main/java/com/springcloud/demo/service/UserService.java
@@ -2,11 +2,11 @@ package com.springcloud.demo.service;
 
 import com.springcloud.demo.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(value="CLOUD-PROVIDER-USER")
 public interface UserService {
 
-    @PostMapping("/user/save")
+    @RequestMapping("/user/save")
     void save(User user);
 }
