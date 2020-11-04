eaee3f23a24c7fe8c0888ca6c6a9a95028d4c938
duan <442492290@qq.com>
2020-11-04 16:19:55 +0800

    添加注解

--- cloud-provider-user/src/main/java/com/springcloud/demo/service/impl/UserServiceImpl.java
+++ cloud-provider-user/src/main/java/com/springcloud/demo/service/impl/UserServiceImpl.java
@@ -4,7 +4,9 @@ package com.springcloud.demo.service.impl;
 import com.springcloud.demo.service.UserService;
 import com.springcloud.demo.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
