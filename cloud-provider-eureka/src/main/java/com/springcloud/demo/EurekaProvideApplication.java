0028a9fbf015e367117914e335d4734044821820
wbl <1762727456@qq.com>
2020-11-04 10:14:19 +0800

    eureka

--- cloud-provider-eureka/src/main/java/com/springcloud/demo/EurekaProvideApplication.java
+++ cloud-provider-eureka/src/main/java/com/springcloud/demo/EurekaProvideApplication.java
@@ -3,12 +3,13 @@ package com.springcloud.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class ConsulProvideApplication {
+@EnableEurekaServer
+public class EurekaProvideApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConsulProvideApplication.class,args);
+        SpringApplication.run(EurekaProvideApplication.class,args);
     }
 }
