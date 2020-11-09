5df2418d2c4059246c4688f934789d1dcada0d19
duan <442492290@qq.com>
2020-11-09 09:39:15 +0800

    调用测试

--- cloud-provider-user/src/main/java/com/springcloud/demo/service/impl/UserServiceImpl.java
+++ cloud-provider-user/src/main/java/com/springcloud/demo/service/impl/UserServiceImpl.java
@@ -19,6 +19,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void findAll() {
-        System.out.println("-------------------------->>>>>>>>>>");
+        System.out.println("-------------------------->>>>>>>>>>service");
     }
 }
